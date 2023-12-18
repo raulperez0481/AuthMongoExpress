fe78380a3c30485b8fffe0dd3f3bd77a14530b27
raulperez0481 <raulperez0481@gmail.com>
2023-12-18 13:09:03 +0100

    sesiones y bcrypt incluido

--- routes/usuarios.mjs
+++ routes/usuarios.mjs
@@ -1,45 +1,89 @@
 import express from "express";
 import db from "../db.mjs";
+import bcrypt from "bcrypt";
+import session from 'express-session';
 
 const router = express.Router();
+
+router.use(session({
+    secret: 'mi-secreto',
+    resave: false,
+    saveUninitialized: true 
+  }));
+  
+  function requireLogin(req, res, next) {
+    if (req.session.user) {
+      console.log('ID de sesión:', req.sessionID);
+      next();
+    } else {
+      res.redirect('/usuarios/login?error=DONDE VAS FLIPAO¡¡¡¡¡');
+    }
+  }
 
 router.get('/login', (req, res) => {
     const error = req.query.error;
     res.render('login', { error });
 });
 
-router.post('/login', async(req, res) => {
-    
+router.post('/login', async (req, res) => {
     try {
-      const { user, pass } = req.body;
+        const { user, pass } = req.body;
 
-      // Verifica si el usuario ya existe
-      const existingUser = await db.collection('usuarios').findOne({ user, pass });
-      console.log("El valor de existingUser es:",existingUser);
+        // Busca el usuario por nombre de usuario
+        const existingUser = await db.collection('usuarios').findOne({ user });
 
-      if(existingUser) {
-        res.redirect('/usuarios/gestion');
-      } else {
-      res.redirect('/usuarios/login?error=Nombre de usuario o contraseña incorrectos');
-      }
-    }
-       catch (error) {   
-        res.status(500).send('Error al buscar el usuario',error);   
+        if (existingUser) {
+            // Compara la contraseña proporcionada con la contraseña almacenada en la base de datos
+            const passwordMatch = await bcrypt.compare(pass, existingUser.pass);
+
+            if (passwordMatch) {
+                //inicializo variables de sesion
+                req.session.user = {
+                    id: user._id,
+                    name: user.name
+                };
+                // Contraseña válida, redirige a la página de gestión
+                res.redirect('/usuarios/gestion');
+            } else {
+                // Contraseña incorrecta, redirige con un mensaje de error
+                res.redirect('/usuarios/login?error=Nombre de usuario o contraseña incorrectos');
+            }
+        } else {
+            // Usuario no encontrado, redirige con un mensaje de error
+            res.redirect('/usuarios/login?error=Nombre de usuario o contraseña incorrectos');
+        }
+    } catch (error) {
+        console.error('Error al buscar el usuario:', error);
+        res.status(500).send('Error interno del servidor');
     }
 });
+
+
+router.get('/logout', (req, res) => {
+    req.session.destroy((err) => {
+      if (err) {
+        console.log(err);
+      }
+      res.redirect('/usuarios/login');
+    });
+  });
 
 router.post('/crearUsuario', async (req, res) => {
     try {
         const { user, pass } = req.body;
 
         // Verifica si el usuario ya existe
-        const existingUser = await db.collection('usuarios').findOne({ user, pass });
+        const existingUser = await db.collection('usuarios').findOne({ user});
         console.log("El valor de existingUser es:",existingUser);
 
         if (existingUser) {
             res.status(400).send('El usuario ya existe');
         } else {
-            const result = await db.collection('usuarios').insertOne({ user, pass });
+           // Encripta la contraseña antes de almacenarla
+           const hashedPassword = await bcrypt.hash(pass, 10);
+
+           // Inserta el nuevo usuario en la base de datos
+           const result = await db.collection('usuarios').insertOne({ user, pass: hashedPassword });           
             if(result.acknowledged) {
                 res.status(201).send('Usuario creado exitosamente');
             } else {
@@ -52,7 +96,7 @@ router.post('/crearUsuario', async (req, res) => {
     }
 });
 
-router.get('/gestion', (req, res) => {
+router.get('/gestion', requireLogin, (req, res) => {
     res.render('gestion');
 });
 
